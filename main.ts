5378c79d420dd187ca1c1aea403bf3c6e67ed8c5
Camillatihi <100693218+Camillatihi@users.noreply.github.com>
2022-03-01 14:05:12 +0100

    Oppdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,75 @@
 basic.forever(function () {
-	
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        # # # # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . # .
+        # # . # .
+        # . # # .
+        # . . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . # .
+        # # . # .
+        # . # # .
+        # . . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . . #
+        # # . # #
+        # . # . #
+        # . . . #
+        `)
+    basic.showLeds(`
+        # # # . .
+        # . . . .
+        # # # . .
+        # . . . .
+        # # # . .
+        `)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # # # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
+        `)
+    basic.showString("nammis")
+    basic.showIcon(IconNames.Heart)
+    music.startMelody(music.builtInMelody(Melodies.Wedding), MelodyOptions.Forever)
 })
